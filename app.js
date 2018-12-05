49c8b11a69b2086578166c0474a334c26543fa59
Долгов Денис <d.dolgov@uni-eng.ru>
2018-12-05 11:03:28 +0300

    mkdir

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const jsonParser = express.json();
 
 app.use('/classes', express.static(__dirname + "/public/classes"));
 
-app.use('/login', express.static(__dirname + "/public/login"));
+app.get('/login', express.static(__dirname + "/public/login"));
 
 app.use('/add', express.static(__dirname + "/add"));
 
@@ -31,27 +31,72 @@ app.post("/login", urlencodedParser, function(req, res){
     res.send(`${req.body.userLogin} - ${req.body.userPassword}`);
 });
 
+
 app.post('/', jsonParser, function(req, res){
     if(!req.body) return res.sendStatus(400);
     console.log("req.body = " ,req.body);
     var jsonCard = (req.body);
-    
-    var dir = __dirname + "\\" + req.body.nameClass;
-    console.log(dir);
-    if (!fs.existsSync(dir)){ 
+
+    var dir = __dirname + "/public/classes/" + req.body.nameClass;
+    console.log("dir = ", dir);
+    if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
     };
-    
+
     var meta = req.body.cardMeta;
-    fs.writeFileSync(dir + '\meta.json', meta);
+    fs.writeFileSync(dir + '/meta.json', meta);
     //fs.writeFile(dir + '/meta.json', jsonCard[2]);
-    
-    
-   
-    
 });
+
+//получение списка мета объектов
+//app.get("/classes", function(req, res){
+
+    var promise = new Promise((res, rej) =>{
+        res(0);
+        rej(1);
+    });
+    var dir = './public/classes/';
+    fs.readdir(dir, function(err, files){
+        if (err){
+            console.log("fs.readdir error = ", err);
+            return(1);
+        };
+        if (files){
+            console.log("fs.readdir files = ", files);
+            
+            
+            for (var i = 0; i < files.length; i++){
+                
+                var path = files[i];
+                console.log('path = ', path);                
+                
+                fs.stat(dir + files[i], function(err, stats){                    
+                    console.log(i, files[i]);
+                    if (err){
+                        console.log("fs.stat error = ", err);
+                        return(1);
+                    };
+                    if (stats.isFile()) {
+                        console.log('    file');
+                    }
+                    if (stats.isDirectory()) {
+                        console.log(dir + path, '    directory');
+                        
+                    }
+                    /*
+                    if (stats){
+                        
+                        console.log("fs.stat = ", stats);
+                    } 
+                    */
+                });                
+            }
+        }
+    });
+//})
+
 
 app.listen(3000, function(){
     console.log("Started server node.js port: 3000");
 });
-//
+//fs.console.dir();
